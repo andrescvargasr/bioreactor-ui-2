137cc0675f389da93264f6d725b0612df9193a9f
andrescvargasr <andresc.vargasr@gmail.com>
2023-06-24 17:46:18 +0200

    ops: setting eslint and prettier
    Signed-off-by: andrescvargasr <andresc.vargasr@gmail.com>

--- .eslintrc.cjs
+++ .eslintrc.cjs
@@ -1,14 +1,32 @@
 module.exports = {
-  env: { browser: true, es2020: true },
+  env: {
+    browser: true,
+    es2021: true,
+  },
   extends: [
     'eslint:recommended',
-    'plugin:@typescript-eslint/recommended',
-    'plugin:react-hooks/recommended',
+    'standard-with-typescript',
+    'plugin:react/recommended',
+    'prettier',
   ],
-  parser: '@typescript-eslint/parser',
-  parserOptions: { ecmaVersion: 'latest', sourceType: 'module' },
-  plugins: ['react-refresh'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+    project: './tsconfig.json',
+  },
+  plugins: ['react'],
   rules: {
-    'react-refresh/only-export-components': 'warn',
+    'react/react-in-jsx-scope': 'off',
   },
 }
