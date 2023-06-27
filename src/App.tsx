402d66117d62cf9fd4aafeb6f2d2545ec97723fe
andrescvargasr <andresc.vargasr@gmail.com>
2023-06-28 00:20:12 +0200

    ops: check NavBar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,13 @@
-import { NavBar } from './components'
+// import { NavBar } from './components'
 
 function App(): JSX.Element {
   return (
     <div className='w-screen h-screen flex overflow-hidden flex-col lg:flex-row'>
-      <NavBar />
+      {/* <NavBar /> */}
+      <p>Hello</p>
       <div className='w-full h-full overflow-scroll bg-gray-50'>
         {/* <AppContent /> */}
+        <p>World!</p>
       </div>
     </div>
   )
--- src/components/common/NavBar.tsx
+++ src/components/common/NavBar.tsx
@@ -1,40 +1,40 @@
 import { useState } from 'react'
-import { NavLink } from 'react-router-dom'
+// import { NavLink } from 'react-router-dom'
 
 // import { SvgOutlineMenu, SvgOutlineX, VerticalNavigation } from '../tailwind-ui'
 import useMedia from '../../hooks/useMedia'
-import { routes, getCurrentRoute } from '../../navigation/routeHelper'
+// import { routes, getCurrentRoute } from '../../navigation/routeHelper'
 
 // Helper func: get only routes which has inNavbar=true
-const getNavbarOptions = (routes, onClick) =>
-  routes.reduce((routeList, route) => {
-    if (route.inNavbar) {
-      const { id, label, value, icon } = route
-      const renderOption =
-        route.component || route.render
-          ? (children, option) => (
-              <NavLink to={option.value} exact onClick={onClick}>
-                {children}
-              </NavLink>
-            )
-          : undefined
+// const getNavbarOptions = (routes, onClick) =>
+//   routes.reduce((routeList, route) => {
+//     if (route.inNavbar) {
+//       const { id, label, value, icon } = route
+//       const renderOption =
+//         route.component || route.render
+//           ? (children, option) => (
+//               <NavLink to={option.value} exact onClick={onClick}>
+//                 {children}
+//               </NavLink>
+//             )
+//           : undefined
 
-      const options = route.options // if route has sub routes (options)
-        ? getNavbarOptions(route.options)
-        : []
+//       const options = route.options // if route has sub routes (options)
+//         ? getNavbarOptions(route.options)
+//         : []
 
-      routeList.push({
-        type: options.length > 0 ? 'group' : 'option',
-        id,
-        label,
-        value,
-        icon,
-        renderOption,
-        options,
-      })
-    }
-    return routeList
-  }, [])
+//       routeList.push({
+//         type: options.length > 0 ? 'group' : 'option',
+//         id,
+//         label,
+//         value,
+//         icon,
+//         renderOption,
+//         options,
+//       })
+//     }
+//     return routeList
+//   }, [])
 
 //
 // Navbar Component
@@ -51,17 +51,18 @@ const NavBar = (): JSX.Element => {
     setIsOpen(false)
   }
 
-  const navbarOptions = getNavbarOptions(routes, onCloseMenu)
+  // const navbarOptions = getNavbarOptions(routes, onCloseMenu)
 
   return (
     <header className='m-0 p-0 w-full lg:w-max lg:h-full relative  bg-primary-900'>
       {/** Top NavBar in small screens */}
       <div className='px-4 flex flex-row justify-between items-center lg:hidden'>
-        <img
+        {/* <img
           src={process.env.PUBLIC_URL + '/logo.svg'}
           alt='Hackuarium Logo'
           className='h-10 '
-        />
+        /> */}
+        <img src={'./logo.svg'} alt='Hackuarium Logo' className='h-10 ' />
         <button
           type='button'
           className='block focus:outline-none relative z-10'
@@ -69,11 +70,11 @@ const NavBar = (): JSX.Element => {
             setIsOpen(!isOpen)
           }}
         >
-          {isOpen ? (
+          {/* {isOpen ? (
             <SvgOutlineX className='text-gray-300 hover:text-white focus:text-white h-6 w-6' />
           ) : (
             <SvgOutlineMenu className='text-gray-300 hover:text-white focus:text-white h-6 w-6' />
-          )}
+          )} */}
         </button>
       </div>
 
@@ -88,26 +89,26 @@ const NavBar = (): JSX.Element => {
       >
         {!isSmallScreen && (
           <div className='w-full flex justify-center pb-8 px-4 '>
-            <img
+            {/* <img
               src={process.env.PUBLIC_URL + '/logo.svg'}
               alt='Hackuarium Logo'
               className='h-24'
-            />
+            /> */}
           </div>
         )}
-        <VerticalNavigation
+        {/* <VerticalNavigation
           onSelect={onCloseMenu}
           // selected={selected}
           selected={getCurrentRoute(routes)}
           options={navbarOptions}
-        />
+        /> */}
       </nav>
 
       {/** on small screens: exit navbar when clicking anywhere in the screen */}
       {isSmallScreen && isOpen && (
         <button
           className='fixed inset-0 h-full w-full cursor-default focus:outline-none'
-          tabIndex='-1'
+          tabIndex={-1}
           onClick={onCloseMenu}
         />
       )}
